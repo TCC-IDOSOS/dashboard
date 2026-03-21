7aea127d696f0612b0bfe98c4b1f69047eadda92
alan-konhevalic <alan.patrick.konhevalic@gmail.com>
2026-03-21 09:55:15 -0300

    TCC-HU013 feat: atualiza arquivos de configs base do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "usuarios", pathMatch: "full" },
+    { path: "usuarios", loadComponent: () => import("./pages/user-list/user-list") },
+    { path: "perfil", loadComponent: () => import("./pages/perfil-usuario/perfil-usuario") },
+];
